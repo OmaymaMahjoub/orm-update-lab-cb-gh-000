3c3442cf2b86478167f672a9f4a3a71c76eb188b
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-18 19:55:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,45 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name,:grade
+
+  attr_reader :id
+
+  def self.create_table
+    sql="CREATE TABLE students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    );"
+    D[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql="DROP TABLE students"
+    D[:conn].execute(sql)
+  end
+
+  def save(name,grade)
+    sql="INSERT INTO students(name,grade) VALUES(?,?)"
+    D[:conn].execute(sql)
+    sql1="SELECT id FROM students ORDER BY id DESC LIMIT 1"
+    @id=D[:conn].execute(sql1)
+  end
+
+  def self.create(name,grade)
+    @name=name
+    @grade=grade
+    save(name,grade)
+  end
+
+  def self.new_from_db(row)
+    s=Students.new
+    s.name(row[1])
+    s.grade(row[2])
+    return s 
+  end
+
+  
 
 
 end
